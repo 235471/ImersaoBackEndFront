d95c441a250519e6c3c928a9ad742d951b06f8d9
Israel <ioliveirap90@gmail.com>
2024-11-25 23:05:43 -0300

    env without eval()

--- assets/js/fetchApis.js
+++ assets/js/fetchApis.js
@@ -1,9 +1,9 @@
-let { API_URL } = process.env;
+const apiUrl = process.env.API_URL;
 
 // Função para buscar os dados do endpoint
 export default async function fetchImages() {
   try {
-    const response = await fetch(process.env.API_URL); // Usando a URL importada
+    const response = await fetch(apiUrl); // Usando a URL importada
     const data = await response.json();
     return data;
   } catch (error) {
